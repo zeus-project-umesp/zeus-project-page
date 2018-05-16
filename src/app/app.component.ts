bfcdd2be2953161a2c4b934a92d7776339e1bbd9
Felipe Francischetto <felipefrancischetto@hotmail.com>
2018-05-15 22:19:58 -0300

    chore: added libs and setups @felipefrancischetto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/platform-browser';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+
+  public navIsFixed = false;
+  public closeResult: string;
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private modalService: NgbModal) {}
+
+  @HostListener('window:scroll', []) onWindowScroll() {
+    const number = window.pageXOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    if (number > 600) {
+      this.navIsFixed = true;
+    } else if (this.navIsFixed && number < 600) {
+      this.navIsFixed = false;
+    }
+  }
+
+  open(content) {
+    this.modalService.open(content).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 import { AppComponent } from './app.component';
-
+import { ComponentsModule } from './shared/components';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    ComponentsModule,
+    BrowserModule,
+    NgbModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
